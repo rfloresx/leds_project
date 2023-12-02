c7e5bd754d0231eefa511ec9a4995cbf77b12f81
Roberto Flores <otrebor4@yahoo.com>
2023-12-02 10:28:07 -0600

    Update Color Palette.
    Add Support for color correction

--- led_sim.py
+++ led_sim.py
@@ -1,61 +1,195 @@
+import sys
+import board
+import palette
 import numpy as np 
+import neopixel
+import random
+import time
 
-colors = [
-    (0x00,0x00,0x00),
-    (0xff,0x00,0x00),
-    (0x00,0xff,0x00),
-    (0x00,0x00,0xff)
-]
-
-class Palette:
-    def __init__(self, colors, res = 100000, colors_map=None):
-        if colors_map is None:
-            n = len(colors)
-            colors_map = np.linspace(0,res,n, endpoint=True)
-        self.res = res
-        self.xp = colors_map
-        self.fp = np.array(colors, dtype =np.int16)
-        self.rp = self.fp[:,0]
-        self.gp = self.fp[:,1]
-        self.bp = self.fp[:,2]
-
-    def interp(self, data):
-        val_r = np.interp(data, self.xp, self.rp)
-        val_g = np.interp(data, self.xp, self.gp)
-        val_b = np.interp(data, self.xp, self.bp)
-        return np.column_stack((val_r, val_g, val_b)).astype(np.int16)
-
+class SimBase:
+    def __init__(self, **kwargs):
+        self.rng = np.random.default_rng()
+        self.leds_count = kwargs.get('leds_count')
+        self.leds = np.zeros([self.leds_count,3], dtype = np.float16)
     
+    def update(self):
+        pass
+
+class HeatSim(SimBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.min_heat = kwargs.get('min_heat', 0)
+        self.max_heat = kwargs.get('max_heat', 255)
+        self.heat = self.rng.integers(self.min_heat, self.max_heat, self.leds_count, dtype=np.int16)
+
+        self.palette = kwargs.get('palette')
+
+    def update(self):
+        super().update()
+
+    def get_leds(self):
+        self.heat = np.clip(self.heat, self.min_heat, self.max_heat)
+        self.leds = self.palette.interp(self.heat)
+        return self.leds
+
+class FireSim(HeatSim):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cold_down = kwargs.get('cold_down', 0)
+        self.sparks = kwargs.get('sparks', 3)
+        self.spark = kwargs.get('spark', self.max_heat - self.max_heat//10)
+
+    def update(self):
+        super().update()
+        self.heat += self.cold_down
+        for n in range(self.sparks):
+            j = random.randint(0,self.leds_count-1)
+            self.heat[j] = self.spark
+
 
 class SparkSim:
-
     def __init__(self, leds_count):
         self.rng = np.random.default_rng()
         self.leds_count = leds_count
-        self.heat = np.zeros(self.leds_count, dtype = np.float16)
+        self.res = 3000
+        # self.heat = np.zeros(self.leds_count, dtype = np.float16)
+        self.heat = self.rng.integers(0, self.res, self.leds_count, dtype = np.int16)
         self.leds = np.zeros([self.leds_count,3], dtype = np.float16)
         self.color = [
             (0x00, 0x00, 0x00),
-            (0xff, 0x00, 0x00),
-            (0x00, 0xff, 0x00),
-            (0x00, 0x00, 0xff),
-            (0xff, 0x00, 0xff),
-            (0x00, 0xff, 0xff),
+            # (0xff, 0x00, 0x00),
+            # (0x00, 0xff, 0x00),
+            # (0x00, 0x00, 0xff),
+            # (0xff, 0x00, 0xff),
+            # (0x00, 0xff, 0xff),
             (0xff, 0xff, 0xff),
         ]
-
-        self.palette = Palette(self.color, 300)
+        self.scale = 30
+        self.palette = palette.Palette(self.color, self.res)
 
     def update(self):
-        hits = self.rng.integers(-1, 1, self.leds_count, dtype = np.int16, endpoint=True) * 3
+        hits = self.rng.integers(-1, 1, self.leds_count, dtype = np.int16, endpoint=True) * self.scale
         self.heat += hits
         self.heat = np.clip(self.heat, 0, self.palette.res)
         self.leds = self.palette.interp(self.heat)
-        
+
+class HexColors:
+    COLOR_MAP = [
+        ("BLACK","#000000"),
+        ("WHITE","#FFFFFF"),
+        ("RED","#FF0000"),
+        ("GREEN","#00FF00"),
+        ("BLUE","#0000FF"),
+        ("ORANGE","#FF8000"),
+        ("YELLOW","#FFFF00"),
+        ("LIME","#80FF00"),
+        ("AQUA","#00FF80"),
+        ("CYAN","#00FFFF"),
+        ("OCEAN","#0080FF"),
+        ("VIOLET","#8000FF"),
+        ("MAGENTA","#FF00FF"),
+        ("RASPBERRY","#FF0080")
+    ]
+    BLACK="#000000"
+    WHITE="#FFFFFF"
+    RED="#FF0000"
+    GREEN="#00FF00"
+    BLUE="#0000FF"
+    ORANGE="#FF8000"
+    YELLOW="#FFFF00"
+    LIME="#80FF00"
+    AQUA="#00FF80"
+    CYAN="#00FFFF"
+    OCEAN="#0080FF"
+    VIOLET="#8000FF"
+    MAGENTA="#FF00FF"
+    RASPBERRY="#FF0080"
+
+    def get(index):
+        return HexColors.COLOR_MAP[index][1]
+    def get_name(index):
+        return HexColors.COLOR_MAP[index][0]
+
+
+
 if __name__ == "__main__":
-    ss = SparkSim(5)
-    while True:
-        ss.update()
-        print(ss.leds)
+    leds_count = 100
+    # p = palette.Palette.from_hex("#FF0000")
+    p = palette.Palette(palette.Palettes.ALL, color_correction = (255, 120, 120))
+    # p = palette.Palette([(255,0,0), (255, 255,0)], color_correction = (255, 120, 180))
+
+    p.set_res(leds_count)
+    ss = FireSim(leds_count = leds_count, palette=p, cold_down=-1, sparks=1, spark=255, min_heat=100, max_heat=240)
+    pixels = neopixel.NeoPixel(board.D18, leds_count, brightness=1, auto_write=False, pixel_order=neopixel.RGB)
+
+    if False:
+        delta = np.linspace(0,leds_count, leds_count)            
+        colors = p.interp(delta)
+        colors_fixed = p.correct_color(colors)
+        pixels[:] = colors_fixed
+        pixels.show()
+        for line in sys.stdin:
+            try:
+                index = int(line)
+                print(f"Sellected {index}")
+                print(f"Color{colors[index]} Color_G{colors_fixed[index]}")
+                break
+            except:
+                print(f"Err: {line}")
+    
+    if False:
+        for line in sys.stdin:
+            if "q" == line.rstrip():
+                break
+            try:
+                rgb = palette.str_to_rgb(line)
+                color = p.correct_color([rgb])[0]
+                pixels.fill(color)
+                pixels.show()
+            except Exception as e:
+                print(F"Err: {line} {e}")
+                raise e
+    if True:
+        for line in sys.stdin:
+            try:
+                index = int(line)
+                hex_color = HexColors.get(index)
+                color = palette.str_to_rgb(hex_color)
+                color = p.correct_color([color])[0]
+                print(f"{index} {color} {hex_color} {HexColors.get_name(index)}")
+                pixels.fill(color)
+                pixels.show()
+
+            except Exception as e:
+                print(f"Err: {line} {e}")
+                raise e
+# 125, 30, 0 Orange
+# 125,53,0 Yello
+# 0,55,10 
+# 0,55,40 # Cyan 
 
 
+# time.sleep(100)
+# for i in range(1000):
+        
+        
+
+        # print(i, c, g)
+        # pixels[0:25] = [c for i in range(25)]
+        # pixels[25:50] = [g for i in range(25)]
+        # pixels.show()
+        # time.sleep(.1)
+    
+        # heat = np.roll(heat, 1)
+        # heat[0] = i
+        # pixels[:] = p.interp(heat)
+        # pixels.show()
+        # time.sleep(.1)
+        #255,80,0 = YELLOW
+        # 
+        # self.leds = p.interp_grb(i)
+    # while True:
+        
+    #     ss.update()
+    #     pixels[:] = ss.get_leds()
+    #     pixels.show()
--- palette.py
+++ palette.py
@@ -1,22 +1,137 @@
 import numpy as np 
 
+def hex_to_rgb(hex):
+    h = hex.lstrip("#")
+    return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+def str_to_rgb(txt):
+    try:
+        tokens = txt.split(',')
+        return (int(tokens[0]), int(tokens[1]), int(tokens[2]))
+    except:
+        pass
+    try:
+        return hex_to_rgb(txt)
+    except:
+        pass
+    raise Exception("Invalid Format")
+
+def hexlst_to_rgblst(lst):
+    items = []
+    for l in lst:
+        items.append(hex_to_rgb(l))
+    return items
+
+
+
 class Palette:
-    def __init__(self, xp, fp):
-        self.xp = xp
-        self.fp = np.array(fp,dtype =np.int16)
+    GAMMA = [
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+        1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+        2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+        5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+        10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+        17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+        25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+        37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+        51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+        69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+        90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+        115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+        144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+        177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+        215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
+    ]
+
+    def __init__(self, colors, res = 100000, colors_map=None, color_correction = (255, 255, 255)):
+        if colors_map is None:
+            n = len(colors)
+            colors_map = np.linspace(0,res,n, endpoint=True)
+        self.res = res
+        self.xp = colors_map
+        self.fp = np.array(colors, dtype =np.int16)
         self.rp = self.fp[:,0]
         self.gp = self.fp[:,1]
         self.bp = self.fp[:,2]
+        self.gama_val = np.array(Palette.GAMMA, dtype = np.int16) 
+        self.gama_x = np.linspace(0, len(self.gama_val), len(self.gama_val), endpoint=False)
+        self.correction = color_correction
 
-    def interp(self, data):
+    def set_res(self, res):
+        self.xp = np.linspace(0,res,len(self.fp), endpoint=True)
+
+    def interp_ext(self, data):
         val_r = np.interp(data, self.xp, self.rp)
         val_g = np.interp(data, self.xp, self.gp)
         val_b = np.interp(data, self.xp, self.bp)
-        return np.column_stack((val_r, val_g, val_b)).astype(np.int16)
+        return (val_r, val_g, val_b)
+    
+    def correct_color(self, data):
+        data = np.array(data)
+        data = (data * self.correction)/255
+        data = np.clip(data, 0, 255)
+        return data.astype(np.int16)
 
+    def apply_gammas(self, data):
+        return np.interp(data, self.gama_x, self.gama_val).astype(np.int16)
+    
+    def interp(self, data):
+        return np.column_stack(self.interp_ext(data)).astype(np.int16)
+
+    def apply_gamma(self, color):
+        return self.apply_gammas([color])[0]
+
+    def get_color(self, data):
+        return self.interp([data])[0]
+
+    def interp_grb(self, data):
+        val_r,val_g,val_b = self.interp_ext(data)
+        return np.column_stack((val_g, val_r, val_b)).astype(np.int16)
+
+    def from_color(color):
+        return Palette([(0,0,0), color])
+
+    def from_hex(hex_color):
+        return Palette.from_color(hex_to_rgb(hex_color))
+
+class Palettes:
+    WHITE=hexlst_to_rgblst(["#000000", "#FFFFFF"])
+    RED=hexlst_to_rgblst(["#000000", "#FF0000"])
+    GREEN=hexlst_to_rgblst(["#000000", "#00FF00"])
+    BLUE=hexlst_to_rgblst(["#000000", "#0000FF"])
+    ALL=hexlst_to_rgblst([
+        "#FF0000",
+        "#FFFF00",
+        "#00FF00",
+        "#00FFFF",
+        "#0000FF",
+        "#FF00FF",
+        "#FF0000",
+    ])
+    FIRE=hexlst_to_rgblst([
+        "#000000",
+        "#ff5a00",
+        "#ffffff"
+    ])
+    FIRE_3= hexlst_to_rgblst([
+        "#FFBB5C",
+        "#FF9B50",
+        "#E25E3E",
+        "#C63D2F",
+    ])
+    FIRE_2 = hexlst_to_rgblst([
+        "#ff0000",
+        "#ff5a00",
+        "#ff9a00",
+        "#ffce00",
+        "#ffce00",
+        "#ffe808"
+    ])
+    
 
 def test():
-    collors = [
+    colors = [
         (0x00, 0x00, 0x00),
         (0xFF, 0x00, 0x00),
         (0x00, 0xFF, 0x00),
@@ -24,7 +139,7 @@ def test():
         (0xFF, 0xFF, 0xFF)
     ]
     steps = [0,10,20,30,40]
-    p = Palette(steps, collors)
+    p = Palette(colors, 40)
     t = np.arange(0,41)
     val = p.interp(t)
     print(val)
